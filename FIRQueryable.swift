2f3a48d905ce1a84ab658e3bb5a1ed49d01da707
ryanburns31892 <rburns@chapman.edu>
2017-01-25 00:52:12 -0800

    More adds to queryable

--- FIRQueryable.swift
+++ FIRQueryable.swift
@@ -8,13 +8,56 @@ protocol FIRQueryable
 
 extension FIRQueryable where Self: FIRModel
 {
+    static func All(completion: @escaping ([Self]) -> Void)
+    {
+        self.GetCollectionRef().child(COLLECTION_NAME).observeSingleEvent(of: .value) { (snapshot: FIRDataSnapshot) in
+            
+            completion(self.GetModels(fromContainerSnapshot: snapshot))
+        }
+    }
+    
     static func From(key: String, completion: @escaping (Self) -> Void)
     {
-        let collectionRef = FIRDatabase.database().reference().child(COLLECTION_NAME)
-        
-        collectionRef.child(key).observeSingleEvent(of: .value) { (snapshot: FIRDataSnapshot) in
+        self.GetCollectionRef().child(key).observeSingleEvent(of: .value) { (snapshot: FIRDataSnapshot) in
             
             completion(Self(snapshot: snapshot))
         }
+    }
+    
+    static func Top(completion: @escaping (Self) -> Void)
+    {
+        self.GetCollectionRef().queryLimited(toFirst: 1).observeSingleEvent(of: .childAdded) { (snapshot: FIRDataSnapshot) in
+            
+            completion(Self(snapshot: snapshot))
+        }
+    }
+    
+    static func TopWhere(child path: String, equals value: Any?, completion: @escaping (Self) -> Void)
+    {
+        self.GetCollectionRef()
+            .queryOrdered(byChild: path)
+            .queryEqual(toValue: value)
+            .queryLimited(toFirst: 1)
+            .observeSingleEvent(of: .childAdded) { (snapshot: FIRDataSnapshot) in
+                
+                completion(Self(snapshot: snapshot))
+        }
+    }
+    
+    private static func GetModels(fromContainerSnapshot snapshot: FIRDataSnapshot) -> [Self]
+    {
+        var models: [Self] = []
+        
+        for obj in snapshot.children where obj is FIRDataSnapshot
+        {
+            models.append(Self.init(snapshot: obj as! FIRDataSnapshot))
+        }
+        
+        return models
+    }
+    
+    private static func GetCollectionRef() -> FIRDatabaseReference
+    {
+        return FIRDatabase.database().reference().child(COLLECTION_NAME)
     }
 }
