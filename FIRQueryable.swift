92b276a509351c452cbfc1dadc3f5f1aa73d4283
Ryan Thomas Burns <ryanburns31892@gmail.com>
2017-03-06 19:40:38 -0800

    fixing All() for the 100th time

--- FIRQueryable.swift
+++ FIRQueryable.swift
@@ -19,7 +19,7 @@ extension FIRQueryable where Self: FIRModel
     
     static func All(completion: @escaping ([Self]) -> Void)
     {
-        self.GetCollectionRef().child(COLLECTION_NAME).observeSingleEvent(of: .value) { (snapshot: FIRDataSnapshot) in
+        self.GetCollectionRef().observeSingleEvent(of: .value) { (snapshot: FIRDataSnapshot) in
             
             completion(self.GetModels(fromContainerSnapshot: snapshot))
         }
